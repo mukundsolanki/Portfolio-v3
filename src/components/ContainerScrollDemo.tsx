716e92f741fc81fd859b38ca06148b162c662157
Mukund Solanki <mukundsolanki665@gmail.com>
2024-02-09 20:06:46 +0530

    sample test comp

---
+++ src/components/ContainerScrollDemo.tsx
@@ -0,0 +1,26 @@
+"use client";
+import React from "react";
+import { ContainerScroll } from "@/components/ui/container-scroll-animation";
+
+export function HeroScrollDemo() {
+  return (
+    <div className="flex flex-col overflow-hidden">
+      <ContainerScroll
+        users={users}
+        titleComponent={
+          <>
+            <h1 className="text-4xl font-semibold text-black dark:text-white">
+              Unleash the power of <br />
+              <span className="text-4xl md:text-[6rem] font-bold mt-1 leading-none">
+                Scroll Animations
+              </span>
+            </h1>
+          </>
+        }
+      />
+    </div>
+  );
+}
+
+export const users = [
+];
