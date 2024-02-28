885618debf993ba13c84285d7779ad428c33a0e8
Mukund Solanki <mukundsolanki665@gmail.com>
2024-02-29 00:06:57 +0530

    Changed favicon and title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+"use client"
 import { Projects } from "@/components/Projects";
 import { ExperienceInfo } from "@/components/ExperienceInfo";
 import { Hero } from "@/components/Hero";
 import { ProjectsCards } from "@/components/ProjectsCards";
 import { HeroScrollDemo } from "@/components/ContainerScrollDemo";
 import { Footer } from "@/components/Footer";
+import { ScrollToTop } from "@/components/ScrollToTop";
 
 export default function Home() {
   return (
@@ -14,6 +16,7 @@ export default function Home() {
       <ProjectsCards/>
       <HeroScrollDemo />
       <Footer/>
+      <ScrollToTop/>
     </>
   );
 }
