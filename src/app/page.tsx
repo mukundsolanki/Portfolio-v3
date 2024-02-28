b683174a2b77f9d02bc6ff2d295a6c9f20ce6281
Mukund Solanki <mukundsolanki665@gmail.com>
2024-02-28 23:48:46 +0530

    Updated dep and svg for mask

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import { Projects } from "@/components/Projects";
 import { ExperienceInfo } from "@/components/ExperienceInfo";
 import { Hero } from "@/components/Hero";
 import { ProjectsCards } from "@/components/ProjectsCards";
+import { HeroScrollDemo } from "@/components/ContainerScrollDemo";
+import { Footer } from "@/components/Footer";
 
 export default function Home() {
   return (
@@ -10,6 +12,8 @@ export default function Home() {
       <ExperienceInfo />
       <Projects />
       <ProjectsCards/>
+      <HeroScrollDemo />
+      <Footer/>
     </>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -23,8 +23,46 @@ const config: Config = {
       },
       animation: {
         spotlight: "spotlight 2s ease .75s 1 forwards",
+        first: "moveVertical 30s ease infinite",
+        second: "moveInCircle 20s reverse infinite",
+        third: "moveInCircle 40s linear infinite",
+        fourth: "moveHorizontal 40s ease infinite",
+        fifth: "moveInCircle 20s ease infinite",
       },
       keyframes: {
+        moveHorizontal: {
+          "0%": {
+            transform: "translateX(-50%) translateY(-10%)",
+          },
+          "50%": {
+            transform: "translateX(50%) translateY(10%)",
+          },
+          "100%": {
+            transform: "translateX(-50%) translateY(-10%)",
+          },
+        },
+        moveInCircle: {
+          "0%": {
+            transform: "rotate(0deg)",
+          },
+          "50%": {
+            transform: "rotate(180deg)",
+          },
+          "100%": {
+            transform: "rotate(360deg)",
+          },
+        },
+        moveVertical: {
+          "0%": {
+            transform: "translateY(-50%)",
+          },
+          "50%": {
+            transform: "translateY(50%)",
+          },
+          "100%": {
+            transform: "translateY(-50%)",
+          },
+        },
         spotlight: {
           "0%": {
             opacity: "0",
@@ -70,7 +108,7 @@ function addVariablesForColors({ addBase, theme }: any) {
   let newVars = Object.fromEntries(
     Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
   );
- 
+
   addBase({
     ":root": newVars,
   });
